c07dec8ba9a2da30944e89e6363000081eca7c0e
nidhirawaluni1515-collab <nidhirawaluni1515@gmail.com>
2026-02-18 12:18:44 -0800

    added the question on comments

--- Lab1.java
+++ Lab1.java
@@ -5,6 +5,18 @@ import java.util.ArrayList;
 public class Lab1 {
     public static void main(String[] args) {
         
+
+// Lab 1
+// Programming Assignment
+// Lab 1 part 1 - Array
+// Initiate two integer arrays, x, and y, each containing 5 numbers.
+// Create another array z of length 5, the elements of which are the max of the respective numbers of the two arrays x and y.
+// Organize a nice-looking output to the terminal that looks like the following:
+// Array x = { array x values here separated by coma }
+
+// Array y = { array y values here separated by coma }
+
+// Array z = x + y = { array z values here separated by coma }
         // ===== PART 1: Array =====        
         // Two arrays with 5 numbers
         int[] x = {3, 7, 2, 9, 5};
@@ -39,7 +51,13 @@ public class Lab1 {
             if (i < 4) zStr += ", ";
         }
         System.out.println( "Array z = max(x, y) = { " + zStr +" }");
-        
+//         Lab 1 part 2 - ArrayList
+// Initiate an ArrayList that contain 5 names
+// Switch the first and last letters in each name in the ArrayList. Do not forget that each name begins with a capital letter, but the other letters are lowercase. The names with switched letters should follow the same rule as there are new names.
+// Organize a nice-looking output to the terminal that looks like the following:
+// Names = { list of names here separated by coma }
+
+// Names (switched) = { list of names with switched first and last letters here separated by coma }
         
         // PART 2: ArrayList =====
         
